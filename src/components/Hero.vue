700a399810a3d9f91007eaf334b8bcfce6862aa4
Maya <pchelaamaya@gmail.com>
2022-11-23 14:36:08 +0300

    append new project

---
+++ src/components/Hero.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="hero">
+        <Heroinformation
+        title="Luxury homeware for people who love timeless design quality"
+        description="Shop the new Spring 2022 collection today"
+        link="#"
+        />
+    </div>
+</template>
+
+<script setup>
+import Heroinformation from './Heroinformation.vue';
+</script>
+
+<style lang="scss" scoped>
+.hero {
+    background: url(/img/hero.jpg);
+    height: 704px;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding-right: 80px;
+}
+</style>
